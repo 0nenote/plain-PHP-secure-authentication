f9a35d23fbd343388fc018b9ad506a00710f6f02
kai <kai.mononen91@gmail.com>
2017-01-17 10:32:00 +0100

    validation functions added

--- app/inputvalidator.php
+++ app/inputvalidator.php
@@ -2,7 +2,14 @@
 class inputValidator {
 
 
-
+	public function stripTags($data)
+	{
+		return strip_tags($data);
+	}
+	
+	public function outputEscape($data){
+		return htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
+	}
 	
 }
 
