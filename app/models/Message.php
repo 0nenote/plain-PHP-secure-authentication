c945d9f25662de365dff29c8b8e80dfe7cd608b4
Amanda Jiménez <amanda.jimenez.molina@gmail.com>
2017-01-19 19:03:05 +0100

    Update Message.php

--- app/models/Message.php
+++ app/models/Message.php
@@ -1,16 +1,25 @@
 <?php
-class Guestbook {
-
-
-	public function insertMessage($message){
+class Message extends Model{
 	
+	private $comment;
+
+	public function __construct($id = o, $comment = ''){
+		$this->id = $id;
+		$this->comment = $comment;
+
+	}
+	
+	public function insertMessage($message, $today){
+		$db = Db::getInstance();
+		$stmt = $db->prepare("INSERT INTO comments(comment, date) VALUES (:comment, :date)");
+
+		$stmt->bindParam(':comment', $message);
+		$stmt->bindParam(':date', $today);
+		$stmt->execute();
 	}
 	
 	public function deleteMessage($id){
-	
-	
+		// coger el id y comprar con la table y después borrarlo
 	}
 	
 }
-
-?>
