e9cebff7afcb5eda6403d597a8a2ed24ef173032
Amanda Jiménez <amanda.jimenez.molina@gmail.com>
2017-01-19 11:04:14 +0100

    .

---
+++ public/html/guestbook.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<p>Your comment has been sent</p>
+
+<?php 
+$comment = $_POST['comment'];
+echo $comment; 
+?>
+//abrir conexión
+//coger datos
+//cerrar conexión
+</body>
+</html>
