1840263e99e0d72b9351a1da1bd25ab248217cb2
Amanda Jiménez <amanda.jimenez.molina@gmail.com>
2017-01-19 19:04:23 +0100

    Update index.php

--- app/views/message/index.php
+++ app/views/message/index.php
@@ -13,7 +13,7 @@
 <body>
 	<div class="container">
      
-      <form class="form-signin" method="post" action="guestbook.php">
+      <form class="form-signin" method="post" action="sendmessage">
         <h2 class="form-signin-heading">Please comment</h2>
         <textarea name='comment' id='comment' cols="27" class="input-block-level" placeholder="Your comment..." required></textarea><br />
         <button class="btn btn-large btn-primary" type="submit">Submit</button>
