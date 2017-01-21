0d5932d83d6a0e8dad167eaaa37a3f1156b089d0
Sevvlor <me@sevvlor.com>
2017-01-21 14:31:40 +0100

    Improved codeformatting todo: - make utility classes - remove  incorrect logic from models - improve code quality - Remove deprecated functioncalls

--- app/controllers/controller.php
+++ app/controllers/controller.php
@@ -1,43 +1,56 @@
 <?php
+
+/**
+ * Class Controller
+ */
 class Controller
 {
-    public function model($model){
+    /**
+     * Creates the model, now o
+     * @param $model
+     * @return mixed
+     * @deprecated
+     */
+    public function model ($model)
+    {
+        trigger_error('This function has been deprecated', E_USER_DEPRECATED);
         require_once 'models/' . $model . '.php';
         return new $model();
-    }    
-    
-    public function view($view, $data = []){
-		
-		
+    }
+
+    /**
+     * Renders the view
+     * @param $view
+     * @param array $data
+     */
+    protected function view ($view, $data = [])
+    {
          require_once '../app/views/' . $view . '.php';
     }
-	
-	public function validateInput(){
-		
+
+    /**
+     * Checks if the Captcha is correctly submitted
+     * todo: return boolean on success or failure and throw exception on missing captcha
+     * @return int
+     */
+    protected function validateInput ()
+    {
 		$secretKey = "6LclkxIUAAAAADdEiNz2c3TyTA3OBxrWuknjxHKY";
 		
-		if(isset($_POST['g-recaptcha-response'])){
-			$captcha=$_POST['g-recaptcha-response'];
-        
-		
-			$response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']);
+		if (isset($_POST['g-recaptcha-response']) )
+		{
+			$response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=" . $secretKey . "&response=" . $_POST['g-recaptcha-response'] . "&remoteip=" . $_SERVER['REMOTE_ADDR']);
 			$data = json_decode($response);
 		
-				if(isset($data->success) && $data->success==true){
-					return 1; //everything went better than expected
-				}
-				else{
+				if (isset($data->success) && $data->success==true) {
+					return 1;
+				} else {
 					return 2; // captcha was not checked
 				}
 			
-		}
-		
-		else{
-				
+		} else {
 			return 3; // captcha is missing from site
 		}
-
 	}
-    
     
 }
--- app/models/Message.php
+++ app/models/Message.php
@@ -1,44 +1,61 @@
 <?php
-class Message extends Model{
-	
-	private $comment;
 
-	public function __construct($id = o, $comment = ''){
+/**
+ * Class Message
+ */
+class Message extends Model
+{
+    /**
+     * @var string
+     */
+    private $comment;
+
+    /**
+     * Message constructor.
+     * @param int $id
+     * @param string $comment
+     */
+    public function __construct($id = 0, $comment = '')
+    {
 		$this->id = $id;
 		$this->comment = $comment;
-
+        parent::__construct();//Call the parent constructor
 	}
-	
-	public function insertMessage($message, $today){
-		$db = Db::getInstance();
-		
+
+    /**
+     * @param $message
+     * @param $today
+     * @return bool
+     */
+    public function insertMessage($message, $today)
+    {
 		/********************************/ 			  // XSS mitigation part
-		$strippedMessage= Model::stripTags($message); //strip HTML tags from message 
-		
+		$strippedMessage= Model::stripTags($message); //strip HTML tags from message
 		/********************************/
 		
-		$stmt = $db->prepare("INSERT INTO comments(comment, postDate) VALUES (:comment, :date)");
+		$stmt = $this->databaseConnection->prepare("INSERT INTO comments(comment, postDate) VALUES (:comment, :date)");
 
 		$stmt->bindParam(':comment', $strippedMessage);
 		$stmt->bindParam(':date', $today);
 		
 		return $stmt->execute();
 	}
-	
-	public function deleteMessage($id){
-		$db = Db::getInstance();
-		
-		
+
+    /**
+     * @param $id
+     * @return bool
+     */
+    public function deleteMessage ($id)
+    {
 		/********************************/ 	// XSS mitigation part
 		$strippedId= Model::stripTags($id); //strip HTML tags from message 
 		
 		/********************************/
 		
-		$stmt = $db->prepare("DELETE FROM comments WHERE id = :id");
+		$stmt = $this->databaseConnection->prepare("DELETE FROM comments WHERE id = :id");
 		
 		$stmt->bindParam(':id', $strippedId);
 		return $stmt->execute();
-		
 		
 	}
 	
