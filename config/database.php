b33b88926a32b41f93479311c644e1466e6ece16
Zouf S <soufiane.s@gmail.com>
2017-01-22 16:42:26 +0100

    db connection exception handling

--- config/database.php
+++ config/database.php
@@ -8,11 +8,17 @@
     private function __clone() {}
 
     public static function getInstance() {
-      if (!isset(self::$instance)) {
+        try{
+    if (!isset(self::$instance)) {
           $ini = parse_ini_file('app.ini');
         $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
         self::$instance = new PDO('mysql:host='.$ini['db_host'].';dbname='.$ini['db_name'],$ini['db_user'],$ini['db_pass'], $pdo_options);
       }
       return self::$instance;
-    }
+    }catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+            
+        }
+}
+     
   }
